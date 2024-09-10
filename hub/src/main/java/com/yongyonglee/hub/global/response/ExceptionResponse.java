70ecdc867091f37f8b30113b5173538c87703125
김소이 <thdl9893@ajou.ac.kr>
2024-09-10 22:40:30 +0900

    feat(#8): 응답객체 생성

---
+++ hub/src/main/java/com/yongyonglee/hub/global/response/ExceptionResponse.java
@@ -0,0 +1,20 @@
+package com.yongyonglee.hub.global.response;
+
+import static lombok.AccessLevel.PRIVATE;
+
+import lombok.Builder;
+import lombok.NonNull;
+
+@Builder(access = PRIVATE)
+public record ExceptionResponse(
+//        boolean success,
+        @NonNull
+        String message
+) implements CommonResponse {
+
+    public static ExceptionResponse of(String message) {
+        return ExceptionResponse.builder()
+                                .message(message)
+                                .build();
+    }
+}
