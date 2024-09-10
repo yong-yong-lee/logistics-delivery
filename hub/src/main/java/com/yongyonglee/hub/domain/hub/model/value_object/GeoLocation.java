7bd9a135db4ab669561dad1e103692a5bf12aaa2
김소이 <thdl9893@ajou.ac.kr>
2024-09-10 13:20:24 +0900

    feat(#6): GeoLocation값객체 POINT타입으로 수정

--- hub/src/main/java/com/yongyonglee/hub/domain/hub/model/value_object/GeoLocation.java
+++ hub/src/main/java/com/yongyonglee/hub/domain/hub/model/value_object/GeoLocation.java
@@ -16,19 +16,30 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class GeoLocation {
 
-    @Column(name = "latitude", nullable = false)
-    private Double latitude;
-
-    @Column(name = "longitude", nullable = false)
-    private Double longitude;
+    // PostgreSQL의 POINT 타입으로 매핑
+    @Column(name = "location", nullable = false, columnDefinition = "POINT")
+    private String value;
 
     public GeoLocation(Double latitude, Double longitude) {
-
-        if(latitude == null || longitude == null) {
+        if (latitude == null || longitude == null) {
             throw new HubException(ExceptionMessage.HUB_GEOLOCATION_NOT_VALID);
         }
 
-        this.latitude = latitude;
-        this.longitude = longitude;
+        if (latitude < -90.0 || latitude > 90.0) {
+            throw new HubException(ExceptionMessage.HUB_LATITUDE_OUT_OF_RANGE);
+        }
+        if (longitude < -180.0 || longitude > 180.0) {
+            throw new HubException(ExceptionMessage.HUB_LONGITUDE_OUT_OF_RANGE);
+        }
+
+        this.value = String.format("POINT(%f %f)", longitude, latitude);
+    }
+
+    public Double getLatitude() {
+        return Double.parseDouble(value.substring(value.indexOf(' ') + 1, value.length() - 1));
+    }
+
+    public Double getLongitude() {
+        return Double.parseDouble(value.substring(value.indexOf('(') + 1, value.indexOf(' ')));
     }
 }
