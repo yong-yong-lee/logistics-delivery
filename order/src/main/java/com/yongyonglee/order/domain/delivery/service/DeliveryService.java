5ad5fceb8f24b5c0fe41b402308004b85844a816
dragon0622 <iamdragon.dev@gmail.com>
2024-09-11 11:17:14 +0900

    feat: 메소드 파라미터 변경으로 인한 객체 생성시 내부 파라미터 변경

--- order/src/main/java/com/yongyonglee/order/domain/delivery/service/DeliveryService.java
+++ order/src/main/java/com/yongyonglee/order/domain/delivery/service/DeliveryService.java
@@ -4,7 +4,12 @@ import com.yongyonglee.order.domain.delivery.dto.DeliveryResponse;
 import com.yongyonglee.order.domain.delivery.entity.Delivery;
 import com.yongyonglee.order.domain.delivery.entity.DeliveryStatus;
 import com.yongyonglee.order.domain.delivery.repository.DeliveryRepository;
+import com.yongyonglee.order.domain.order.dto.OrderCreateRequest;
 import com.yongyonglee.order.domain.order.dto.OrderResponse;
+import com.yongyonglee.order.domain.order.entity.Order;
+import com.yongyonglee.order.global.response.CustomException;
+import com.yongyonglee.order.global.response.ErrorCode;
+import java.util.UUID;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,22 +21,31 @@ public class DeliveryService {
     private final DeliveryRepository deliveryRepository;
 
     @Transactional
-    public DeliveryResponse addDelivery(OrderResponse orderResponse) {
+    public DeliveryResponse addDelivery(OrderCreateRequest orderCreateRequest, UUID orderId) {
 
 //        아래부분의 address 조회는 feignClient 이용해야합니다. 현재는 더미 데이터입니다. deliveryRepo가 아니라 vendorRepo.findById(주문요청한 가게 id).getAddress() 해야할듯
-        String address = deliveryRepository.findById(orderResponse.getDemandId()).get()
+        String address = deliveryRepository.findById(orderCreateRequest.getDemandId()).get()
                 .getVendorAddress();
 
         Delivery delivery = Delivery.builder()
-                .orderId(orderResponse.getId())
+                .orderId(orderId)
                 .status(DeliveryStatus.WAITING_AT_HUB)
-                .departureId(orderResponse.getSupplyId())
-                .arrivalId(orderResponse.getDemandId())
+                .departureId(orderCreateRequest.getSupplyId())
+                .arrivalId(orderCreateRequest.getDemandId())
                 .vendorAddress(address)
-                .receiverName(orderResponse.getReceiverName())
-                .receiverSlackId(orderResponse.getReceiverSlackId())
+                .receiverName(orderCreateRequest.getReceiverName())
+                .receiverSlackId(orderCreateRequest.getReceiverSlackId())
                 .build();
 
         return deliveryRepository.save(delivery).toResponse();
+    }
+
+    @Transactional
+    public DeliveryResponse getDelivery(UUID deliveryId) {
+
+        Delivery delivery = deliveryRepository.findById(deliveryId).orElseThrow(()-> new CustomException(
+                ErrorCode.DELIVERY_ID_NOT_FOUND));
+
+        return delivery.toResponse();
     }
 }
