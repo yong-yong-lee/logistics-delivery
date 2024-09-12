1385985ab9cbea91234d8b5a36a25577c5ccff44
김소이 <thdl9893@ajou.ac.kr>
2024-09-13 01:25:08 +0900

    chore(#11): SwaggerConfig 간단한 설정

---
+++ hub/src/main/java/com/yongyonglee/hub/global/config/SwaggerConfig.java
@@ -0,0 +1,40 @@
+package com.yongyonglee.hub.global.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("HUB-SERVICE API")
+                .description("TEAM 용용이 hub 서비스의 hub, hub-route 관련 swagger 페이지입니다.")
+                .version("1.0.0");
+    }
+}
