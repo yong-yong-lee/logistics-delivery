477f7c0e56f8dab07be0764720b8bb863eb673ac
김소이 <thdl9893@ajou.ac.kr>
2024-09-12 13:59:07 +0900

    style(#16): auditing, TimeBase 설정

---
+++ vendor/src/main/java/com/yongyonglee/vendor/global/config/JpaAuditingConfig.java
@@ -0,0 +1,11 @@
+package com.yongyonglee.vendor.global.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "userAuditorAware")
+public class JpaAuditingConfig {
+
+}
