03e3e1b1fd2f4320b5a6a3ccab8bf8eaf52053d3
dragon0622 <iamdragon.dev@gmail.com>
2024-09-13 18:19:11 +0900

    feat: vendorDto 생성

---
+++ order/src/main/java/com/yongyonglee/order/domain/route/VendorResponseDto.java
@@ -0,0 +1,18 @@
+package com.yongyonglee.order.domain.route;
+
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+import lombok.AccessLevel;
+import lombok.Builder;
+
+@Builder(access = AccessLevel.PRIVATE)
+public record VendorResponseDto(
+        UUID vendorId,
+        String vendorName,
+        String vendorCategory,
+        String vendorAddress,
+        UUID hubId,
+        LocalDateTime createdAt
+){}
+
