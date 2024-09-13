7d5ddb6ac6377b6ca7d733b0ff6b0a360e6b108b
dragon0622 <iamdragon.dev@gmail.com>
2024-09-13 18:18:09 +0900

    feat: feignClient 이용 address 조회

--- order/src/main/java/com/yongyonglee/order/domain/delivery/service/DeliveryService.java
+++ order/src/main/java/com/yongyonglee/order/domain/delivery/service/DeliveryService.java
@@ -5,6 +5,8 @@ import com.yongyonglee.order.domain.delivery.entity.Delivery;
 import com.yongyonglee.order.domain.delivery.entity.DeliveryStatus;
 import com.yongyonglee.order.domain.delivery.repository.DeliveryRepository;
 import com.yongyonglee.order.domain.order.dto.OrderCreateRequest;
+import com.yongyonglee.order.domain.route.VendorClient;
+import com.yongyonglee.order.domain.route.VendorResponseDto;
 import com.yongyonglee.order.domain.route.entity.Route;
 import com.yongyonglee.order.domain.route.service.RouteService;
 import com.yongyonglee.order.global.response.CustomException;
@@ -12,6 +14,7 @@ import com.yongyonglee.order.global.response.ErrorCode;
 import java.util.List;
 import java.util.UUID;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -21,13 +24,20 @@ public class DeliveryService {
 
     private final DeliveryRepository deliveryRepository;
     private final RouteService routeService;
+    private final VendorClient vendorClient;
+
+
+    public VendorResponseDto getVendorInfo(UUID vendorId) {
+
+        ResponseEntity<VendorResponseDto> response = vendorClient.getVendor(vendorId);
+
+        return response.getBody();
+    }
 
     @Transactional
     public DeliveryResponse addDelivery(OrderCreateRequest orderCreateRequest, UUID orderId) {
 
-//        아래부분의 address 조회는 feignClient 이용해야합니다. 현재는 더미 데이터입니다. deliveryRepo가 아니라 vendorRepo.findById(주문요청한 가게 id).getAddress() 해야할듯
-        String address = deliveryRepository.findById(orderCreateRequest.getDemandId()).get()
-                .getVendorAddress();
+        String address = getVendorInfo(orderCreateRequest.getDemandId()).vendorAddress();
 
         List<Route> routes = routeService.addRoute(orderCreateRequest);
 
@@ -41,9 +51,6 @@ public class DeliveryService {
                 .receiverSlackId(orderCreateRequest.getReceiverSlackId())
                 .routes(routes)
                 .build();
-
-
-
 
 
         return deliveryRepository.save(delivery).toResponse();
