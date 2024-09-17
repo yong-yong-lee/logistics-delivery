8ebb13dab283f56ddabf1150d4b08018e33bdff2
김소이 <thdl9893@ajou.ac.kr>
2024-09-18 02:12:00 +0900

    chore(#34): responseDto 직렬화 설정

--- hub/src/main/java/com/yongyonglee/hub/domain/hub/dto/response/CreateHubResponseDto.java
+++ hub/src/main/java/com/yongyonglee/hub/domain/hub/dto/response/CreateHubResponseDto.java
@@ -1,6 +1,7 @@
 package com.yongyonglee.hub.domain.hub.dto.response;
 
 import com.yongyonglee.hub.domain.hub.model.Hub;
+import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
 import lombok.AccessLevel;
@@ -14,7 +15,7 @@ public record CreateHubResponseDto(
         Double latitude,
         Double longitude,
         LocalDateTime createdAt
-) {
+) implements Serializable {
 
     public static CreateHubResponseDto from(Hub hub) {
         return CreateHubResponseDto.builder()
